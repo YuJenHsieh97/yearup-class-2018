87463218f13dace1a00347ca6a832dcfbac8cc66
Yu Jen Hsieh <yhsieh@nyc.yearup.org>
2018-11-09 12:57:11 -0500

    Image DOM

--- w03/d5/workshop/main.js
+++ w03/d5/workshop/main.js
@@ -1,16 +1,28 @@
 class Art {
-  constructor(){
-
-  }
-
-  render(){
-    //do things to the DOM here
+  constructor(frame, frame_color,art_url){
+     this.frame = frame;
+     this.color = frame_color;
+     this.url = art_url;
   }
 }
+
+
+  // render(){
+  //   //do things to the DOM here
+  // }
+
 
 class Classical extends Art {
   constructor(){
-    super();
+    super(frame, frame_color,art_url);
   }
 
 }
+
+let newElement = document.createElement('img');
+newElement.src = "http://simpleabstract.com/assets/images/Oil-On-Canvas-Abstract-Art.jpg";
+
+newElement.className = "pictures";
+
+let parent = document.getElementById('image');
+parent.appendChild(newElement);
