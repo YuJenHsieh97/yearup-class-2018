7d4aa4836661509e3f9ab3f6de99cb935521b356
Yu Jen Hsieh <yhsieh@nyc.yearup.org>
2018-11-15 10:19:42 -0500

    For loop for API!

--- w04/d2/workshop/main.js
+++ w04/d2/workshop/main.js
@@ -4,12 +4,13 @@ let url = "https://fizal.me/products.json";
 axios.get(url)
   .then(function (open){
     let first = open.data.products[0].images[0].src;
-    let heading = document.getElementById('image');
-    let img = document.getElementsByTagName('img');
     for (i=0;i<open.data.products.length;i++){
-      let newImg=parent.document.createElement('img');
+      let div = document.getElementById('container');
+      let newImg=document.createElement('img');
       let all = open.data.products[i].images[0].src;
-      newImg.appendChild(all);
+      newImg.src = all;
+      div.appendChild(newImg);
+      // console.log(div);
     }
   });
 
@@ -30,13 +31,13 @@ axios.get(url)
   // let eleven = document.getElementById('11');
   // let i =0;
 
-  // two.src=open.data.products[i].images[0].src;
-  // three.src=open.data.products[i].images[0].src;
-  // four.src=open.data.products[i].images[0].src;
-  // five.src=open.data.products[i].images[0].src;
-  // six.src=open.data.products[i].images[0].src;
-  // seven.src=open.data.products[i].images[0].src;
-  // eight.src=open.data.products[i].images[0].src;
-  // nine.src=open.data.products[i].images[0].src;
-  // ten.src=open.data.products[i].images[0].src;
-  // eleven.src=open.data.products[i].images[0].src;
+  // two.src=open.data.products[1].images[0].src;
+  // three.src=open.data.products[2].images[0].src;
+  // four.src=open.data.products[3].images[0].src;
+  // five.src=open.data.products[4].images[0].src;
+  // six.src=open.data.products[5].images[0].src;
+  // seven.src=open.data.products[6].images[0].src;
+  // eight.src=open.data.products[7].images[0].src;
+  // nine.src=open.data.products[8].images[0].src;
+  // ten.src=open.data.products[9].images[0].src;
+  // eleven.src=open.data.products[10].images[0].src;
