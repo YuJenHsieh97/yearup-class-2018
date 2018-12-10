fb90313655dddcb5c7929e6f4f9fe9e06d24f432
yujenhsieh97 <yhsieh@nyc.yearup.org>
2018-12-10 13:11:06 -0500

    For new workshop

--- w07/d04/classwork/fizal.rb
+++ w07/d04/classwork/fizal.rb
@@ -1,11 +1,21 @@
 require 'sinatra'
+require('forecast_io')
+require('geocoder')
+ForecastIO.api_key = 'a65700d3d69f77123ba1cbece8365328'
+lat_lon =  Geocoder.search("nyc").first.coordinates
+forecast = ForecastIO.forecast(lat_lon[0], lat_lon[1])
 
-get '/random' do
-  ## when someone asks for localhost:3000/random
+get '/weather' do
+  @weather =forecast["currently"]["summary"]
   erb :index
 end
 
-get '/frank-says' do
-  #when someone asks for localhost:3000/frank-says
-  '<h1>Diddy</h1>'
-end
+# get '/random' do
+#   ## when someone asks for localhost:3000/random
+#   erb :index
+# end
+
+# get '/frank-says' do
+#   #when someone asks for localhost:3000/frank-says
+#   '<h1>Diddy</h1>'
+# end
